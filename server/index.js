1103487152c0141bed714605fce49c1ddf37f63c
lee <zhangloo333@gmail.com>
2017-12-15 10:49:56 -0800

    add nodemon to 监控变化，增加app 的middlewhere

--- server/index.js
+++ server/index.js
@@ -8,9 +8,13 @@ const morgan = require('morgan');
 const app = express();
 
 // App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
 
 //server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('server listening on:', port);
+
+//addasd
